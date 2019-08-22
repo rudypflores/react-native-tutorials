3333cb02903c2e12689647ecf39c840f19329ff0
rudy <rudypichola@gmail.com>
2019-08-22 00:47:19 -0500

    Integration of MVC

---
+++ blog/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
+
+
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blog'
+  }
+});
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
---
+++ blog/src/context/BlogContext.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+
+    const blogPosts = [
+        { title: 'Blog post #1' },
+        { title: 'Blog post #2' }
+    ];
+
+    return (
+        <BlogContext.Provider value={blogPosts}>
+            {children}
+        </BlogContext.Provider>
+    );
+};
+
+
+export default BlogContext;
