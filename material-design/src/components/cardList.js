c826a459d0c6fb64effdcd881c1c31b60b45344d
rudy <rudypichola@gmail.com>
2019-08-29 21:50:56 -0500

    completion of card view

--- material-design/src/components/cardList.js
+++ material-design/src/components/cardList.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Card, Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
-const CardList = ({ title, subtitle }) => {
+const CardList = ({ title, subtitle, img }) => {
+
+    const [expanded, setExpanded] = useState(false);
+    const [arrow, setArrow] = useState('keyboard-arrow-down');
+
+    const expand = () => {
+        setExpanded(!expanded);
+        if(!expanded) {
+            styles.cardContent = { ...styles.cardContent, paddingBottom: 150 };
+            setArrow('keyboard-arrow-up');
+        } else {
+            styles.cardContent = { ...styles.cardContent, paddingBottom: 15 };
+            setArrow('keyboard-arrow-down');
+        }
+    };
+
     return (
         <View>
             <Card style={styles.card}>
-                <Card.Title 
-                    title={title} 
-                    subtitle={subtitle}
-                />
-                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                <Card.Actions style={styles.cardButton}>
-                    <Button onPress={() => console.log('cancel')}>CANCEL</Button>
-                    <Button onPress={() => console.log('ok')}>OK</Button>
-                </Card.Actions>
+                <Card.Cover source={img} />
+                <Card.Content style={styles.cardContent}>
+                    <View style={styles.cardInfo}>
+                        <Title style={{ fontWeight:'bold', color:'#1565C0' }}>{title}</Title>
+                        <Paragraph>{subtitle}</Paragraph>
+                    </View>
+                    <View style={styles.cardAction}>
+                        <TouchableOpacity onPress={() => expand()} color='#1565C0'>
+                            <MaterialIcons name={arrow} color='#1565C0' size={35}/>
+                        </TouchableOpacity>
+                    </View>
+                </Card.Content>
             </Card>
         </View>
     );
@@ -23,11 +42,23 @@ const CardList = ({ title, subtitle }) => {
 
 const styles = StyleSheet.create({
     card: {
-        elevation: 2,
-        margin:20,
+        margin: 10,
+        flex:1
     },
-    cardButton: {
-        alignSelf:'flex-end'
+    cardContent: {
+        justifyContent:'space-between',
+        alignItems:'center',
+        flexDirection:'row',
+        marginTop:10,
+        flex:10
+    },
+    cardInfo: {
+        alignSelf:'center',
+        flex:9
+    },
+    cardAction: {
+        alignSelf:'center',
+        flex:1
     }
 });
 
--- material-design/src/screens/CardScreen.js
+++ material-design/src/screens/CardScreen.js
@@ -6,28 +6,30 @@ import CardList from '../components/cardList';
 const CardScreen = () => {
 
     //Card information for list
-    const cardData = [{ title:'#1', subtitle:'number one' }, 
-                      { title:'#2', subtitle:'number two' },
-                      { title:'#3', subtitle:'number three' },
-                      { title:'#4', subtitle:'number four' },
-                      { title:'#5', subtitle:'number five' }];
+    const cardData = [{ title:'Trip to San Cristobal', subtitle:'location in the capital of Guatemala', img:require('../../assets/imgOne.jpg') }, 
+                      { title:'Trip to Panajachel', subtitle:'Skydiving and hiking wild are in Guatemala', img:require('../../assets/imgTwo.jpg') },
+                      { title:'Trip to Hollywood', subtitle:'Hiking area around the famous Hollywood sign', img:require('../../assets/imgThree.jpg') },
+                      { title:'Trip to Itztapa\'s Hot Springs', subtitle:'Hot spring area full of beautiful scenery', img:require('../../assets/imgFour.jpg') },
+                      { title:'Rio Dulce', subtitle:'Famous river in Guatemala with boating and a nice dock area', img:require('../../assets/imgFive.jpg') }];
 
     return (
-        <View>
+        <>
             {/* Missing a keyExtractor */}
             <FlatList 
                 showsVerticalScrollIndicator={false}
                 data={cardData}
+                keyExtractor={(item) => item.title}
                 renderItem={({ item }) => {
                     return (
                         <CardList 
                             title={item.title} 
                             subtitle={item.subtitle}
+                            img={item.img}
                         />
                     );
                 }}
             />
-        </View>
+        </>
     );
 };
 
