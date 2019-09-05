174f05ed67a5b714cf7fcdfa7212de338696657d
rudy <rudypichola@gmail.com>
2019-09-05 07:35:35 -0500

    refactor rudy

---
+++ tracks/src/screens/SigninScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const SigninScreen = () => {
+    return <Text style={{ fontSize:35 }}>SigninScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SigninScreen;
---
+++ tracks/src/screens/TrackCreateScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const TrackCreateScreen = () => {
+    return <Text style={{ fontSize:35 }}>TrackCreateScreen</Text>;
+};
+
+
+const styles = StyleSheet.create({
+
+});
+
+export default TrackCreateScreen;
